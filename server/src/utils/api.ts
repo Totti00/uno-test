fb12eedb2581f9863d4f20fb2adcc616be85010d
Totti00 <gek5800@gmail.com>
2024-02-10 15:44:36 +0100

    trasled api file to .ts

--- server/src/utils/api.ts
+++ server/src/utils/api.ts
@@ -1,9 +1,11 @@
 import GameServer from "./gameServer";
-const { getServer, setServer, deleteServer } = require("./servers");
-const {addPlayer, removePlayer, getPlayer} = require("./playersSockets");
+import { IGameServer, IPlayer } from "./interfaces";
+import {getServer, setServer, deleteServer, isThereServer} from "./servers";
+import {addPlayer, removePlayer, getPlayer, isTherePlayer} from "./playersSockets";
 import { getCard } from "./cards";
+import { Socket } from "socket.io";
 
-function createServer({ serverName }) {
+export function createServer({ serverName }: { serverName: string }): string {
     if (serverName.trim().length < 2) throw new Error();
     const server = new GameServer(serverName);
     const serverId = server.serverId;
@@ -12,23 +14,28 @@ function createServer({ serverName }) {
     return serverId;
 }
 
-function joinServer({
+export function joinServer({
     serverId,
     io,
     player,
     socket,
     cb = () => {},
-}) {
+}: {
+    serverId: string;
+    io: any;
+    player: IPlayer;
+    socket: Socket;
+    cb?: (error: Error | null, playerId?: string) => void;
+}): void {
     const server = getServer(serverId);
 
     if (!server) throw new Error("Server Doesn't Exist");
     if (player.name.trim().length <= 1) throw new Error("Player Name too short");
-    if (server.players.length >= 4 /* server.numberOfPlayers */ )
-        throw new Error("Server is Already full");
+    if (server.players.length >= 4 /* server.numberOfPlayers */) throw new Error("Server is Already full");
 
     let playerId;
     if (socket) {
-        player.socketId = socket.id;
+        player.socketID = socket.id;
         socket.join(serverId);
         playerId = server.joinPlayer(player);
         addPlayer(socket.id, playerId, serverId);
@@ -45,28 +52,28 @@ function joinServer({
     }
 }
 
-function initGame(server, io) {
+export function initGame(server: IGameServer, io: any): void {
     setTimeout(() => {
-        const { nxtPlayer, card} = server.start();
+        const { nxtPlayer, card } = server.start();
 
         const playersToSend = server.players.map((player) => ({
             ...player,
             cards: [], // just empty cards objects
         }));
         for (const player of server.players) {
-            if (player.socketId) {
-                io.to(player.socketId).emit("init-game", {
+            if (player.socketID) {
+                io.to(player.socketID).emit("init-game", {
                     players: playersToSend,
                     cards: player.cards,
                     nxtPlayer,
-                    card
+                    card,
                 });
             }
         }
     }, 2000);
 }
 
-function startGame(serverId, io) {
+export function startGame(serverId: string, io: any): void {
     const server = getServer(serverId);
     if (!server.gameRunning) {
         server.gameRunning = true;
@@ -84,17 +91,16 @@ function startGame(serverId, io) {
     }
 }
 
-function move({ socket, cardId, draw }, io) {
+export function move({ socket, cardId, draw }: { socket: Socket; cardId: string; draw: boolean }, io: any): void {
     const { playerId, serverId } = getPlayer(socket.id);
     const server = getServer(serverId);
     const card = getCard(cardId);
 
     // Check if its my turn
-    if (server.players[server.curPlayer].id !== playerId)
-        throw new Error("Not Your Turn");
+    if (server.players[server.curPlayer].id !== playerId) throw new Error("Not Your Turn");
 
     // Make the move
-    const { nxtPlayer, cardsToDraw, finish, playersFinishingOrder} = server.move(draw, card);
+    const {nxtPlayer, cardsToDraw, finish, playersFinishingOrder} = server.move(draw, card);
 
     //broadcast to all OTHER players
     socket.broadcast.to(serverId).emit("move", {
@@ -110,24 +116,27 @@ function move({ socket, cardId, draw }, io) {
         draw: cardsToDraw?.length,
         cardsToDraw,
     });
-    
+
     if (finish) {
         console.info("API.CJS game finished");
+        server.gameRunning = false;
         io.to(serverId).emit("finished-game", playersFinishingOrder);
     }
 }
 
-function moveSelectableColorCard({ socket, cardId, draw, colorSelected }, io) {
+export function moveSelectableColorCard(
+    { socket, cardId, draw, colorSelected }: { socket: Socket; cardId: string; draw: boolean; colorSelected: string },
+    io: any
+): void {
     const { playerId, serverId } = getPlayer(socket.id);
     const server = getServer(serverId);
     const card = getCard(cardId);
 
     // Check if its my turn
-    if (server.players[server.curPlayer].id !== playerId)
-        throw new Error("Not Your Turn");
+    if (server.players[server.curPlayer].id !== playerId) throw new Error("Not Your Turn");
 
     // Make the move
-    const { nxtPlayer, cardsToDraw, finish, playersFinishingOrder} = server.move(draw, card);
+    const { nxtPlayer, cardsToDraw, finish, playersFinishingOrder } = server.move(draw, card);
 
     //broadcast to all OTHER players
     socket.broadcast.to(serverId).emit("move-selectable-color-card", {
@@ -148,15 +157,16 @@ function moveSelectableColorCard({ socket, cardId, draw, colorSelected }, io) {
 
     if (finish) {
         console.info("API.CJS game finished");
+        server.gameRunning = false;
         io.to(serverId).emit("finished-game", playersFinishingOrder);
     }
 }
 
-function chat({ socket, message }) {
+export function chat({ socket, message }: { socket: Socket; message: string }): void {
     const { serverId } = getPlayer(socket.id);
     const server = getServer(serverId);
 
-    const messages = server.chat(message)
+    const messages = server.chat(message);
 
     //broadcast to other players
     socket.broadcast.to(serverId).emit("chat", {
@@ -167,36 +177,44 @@ function chat({ socket, message }) {
     socket.emit("chat", {
         messages,
     });
-
 }
 
-function getChat(serverId) {
+export function getChat(serverId: string): string[] {
     const server = getServer(serverId);
-
     return server.getChat();
 }
 
-function leaveServer(socket, io) {
-    try {
-        const player = getPlayer(socket.id);
-        const { playerId, serverId } = player;
-        const server = getServer(serverId);
+export function leaveServer(socket: Socket, io: any): void {
 
-        server.leavePlayer(playerId);
-        let connectedPlayer = 0;
-        for (const p of server.players) {
-            if (!p.disconnected) connectedPlayer++;
+    if (isTherePlayer(socket.id)) {
+        const player = getPlayer(socket.id);
+        const { serverId} = player;
+
+        if (isThereServer(serverId)) {
+            const server = getServer(serverId);
+            deleteServer(serverId);
+            socket.leave(serverId);
+            io.to(serverId).emit("players-changed", server.players);
         }
-        if (connectedPlayer === 0) deleteServer(serverId);
+    }
+
+    /*  try {
+
+
+
+        let connectedPlayers = 0;
+         for (const player of server.players) {
+             if (!player.disconnected) connectedPlayers++;
+         }
+         console.info("API.CJS leaveServer connectedPlayers
 
         socket.leave(serverId);
         if (server.gameRunning) io.to(serverId).emit("player-left", playerId);
         else io.to(serverId).emit("players-changed", server.players);
 
-        //if (server.players[server.curPlayer].id === playerId) moveBot(server);
-
         removePlayer(socket.id);
-    } catch (error) {}
+    } catch (error) {
+        console.log(error);
+    } */
 }
 
-export {createServer, joinServer, startGame, leaveServer, move, moveSelectableColorCard, chat, getChat}

da4d05f738aa279305b860907a3759388b555c08
Totti00 <gek5800@gmail.com>
2024-08-21 20:37:22 +0200

    fixed sonarcloud warnings

--- client/src/components/FrontCards.tsx
+++ client/src/components/FrontCards.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import Card from "./Card";
 import { v4 as uuidv4 } from "uuid";
 
+// Funzione per generare numeri casuali sicuri
+function getRandomNumber(min: number, max: number): number {
+  const randomBuffer = new Uint32Array(1);
+  window.crypto.getRandomValues(randomBuffer);
+  const randomNumber = randomBuffer[0] / (0xffffffff + 1);
+  return Math.floor(randomNumber * (max - min + 1)) + min;
+}
+
 const FrontCards: React.FC = React.memo(function () {
   return (
     <>
@@ -12,9 +20,7 @@ const FrontCards: React.FC = React.memo(function () {
             className="card-container"
             key={uuidv4()} //it was key={idx} before
             style={{
-              transform: `translate(${Math.random() * 20 - 10}px,${
-                Math.random() * 20 - 10
-              }px)`,
+              transform: `translate(${getRandomNumber(-10, 10)}px, ${getRandomNumber(-10, 10)}px)`,
             }}
           >
             <Card />

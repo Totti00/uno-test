7a47e102a3648772cd6f6cf3cac7aaf950a5b8b4
sbertaccini <84337195+sbertaccini@users.noreply.github.com>
2024-11-04 18:16:18 +0100

    add logo in homepage

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -16,18 +16,30 @@ const Home = () => {
     };
 
     return (
-        <div style={{ width: "100%", height: "100%" }}>
-
-            <Row align="middle"
-                 gutter={[0, 16]}
-                 style={{ position: "absolute", bottom: "18%", width: "50%", textAlign: "center", left: 0, right: 0, marginLeft: "auto", marginRight: "auto" }}
-                 justify="space-between"
+        <div style={{ width: "100%", height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+            <Row justify="center" style={{ width: "100%", marginTop: "-17vh" }}>  {/* Adjust marginTop to translate upwards */}
+                <img src="assets/images/uno-logo.png" alt="UNO Logo" style={{ width: "35%" }}/>
+            </Row>
+            <Row
+                align="middle"
+                gutter={[0, 16]}
+                style={{
+                    width: "50%",
+                    textAlign: "center",
+                    position: "absolute",
+                    bottom: "18%",
+                    left: 0,
+                    right: 0,
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                }}
+                justify="space-between"
             >
                 <Button style={{ width: 150, height: 50, fontSize: 18 }} size="large" onClick={onPlayOnlineForJoin}>Join lobby</Button>
                 <Button style={{ width: 150, height: 50, fontSize: 18 }} size="large" onClick={onPlayOnlineForCreate}>Create lobby</Button>
                 <Button style={{ width: 150, height: 50, fontSize: 18 }} size="large" onClick={() => navigate("/rules")}>Rules</Button>
             </Row>
-        </div >
+        </div>
     )
 }
 export default Home
